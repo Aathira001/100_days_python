a24de1837db3caf7aaf0f8f5ac957c3ab82188c8
Aathira001 <aathirastarz@gmail.com>
2023-01-03 17:55:02 -0500

    NATO phonetics alphabet - adding try and except conditions

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -3,6 +3,16 @@ import pandas as pd
 nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-user_word = input("Enter a Word: ").upper()
-codes_for_user = [nato_dictionary[letter] for letter in user_word]
-print(f"Here are the codes for your name: \t {codes_for_user}")
+
+def generate_phonetic():
+    user_word = input("Enter a Word: ").upper()
+    try:
+        codes_for_user = [nato_dictionary[letter] for letter in user_word]
+    except KeyError:
+        print(f"Sorry, {user_word} contains a character that is not a letter. Please enter a valid name ")
+        generate_phonetic()
+    else:
+        print(f"Here are the codes for your name: \t {codes_for_user}")
+
+
+generate_phonetic()
