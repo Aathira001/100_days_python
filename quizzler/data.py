29ca936a0056286b788815498e9ba50390206e8a
Aathira001 <aathirastarz@gmail.com>
2023-01-17 11:34:59 -0500

    Quizzler - adding data retrieval code

---
+++ quizzler/data.py
@@ -0,0 +1,13 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+url = "https://opentdb.com/api.php"
+
+response = requests.get(url=url, params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
